f47d51928b230a0b2a5e3215471250a6d7a55e54
ilaylyk <ilay_lyk@mail.ru>
2022-09-14 03:18:59 +0300

    uput

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,14 +6,30 @@ public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
         List<String> findDeveloperProject = new ArrayList<>();
 
-        for(String person : projects.keySet()){
-            for (String pet : projects.get(person)){
-                if (Objects.equals(pet, developer)){
-                    findDeveloperProject.add(person);
+        for (String projectsDeveloper : projects.keySet()) {
+            for (String pet : projects.get(projectsDeveloper)) {
+                if (Objects.equals(pet, developer)) {
+                    findDeveloperProject.add(projectsDeveloper);
                 }
             }
         }
+        Comparator<String> stringComparator = new FindDeveloperProjectComparator();
+        findDeveloperProject.sort(stringComparator);
+
         Collections.reverse(findDeveloperProject);
         return findDeveloperProject;
+    }
+
+    public static class FindDeveloperProjectComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1.compareTo(o2) == 1) {
+                return 0;
+            } else if (o1.compareTo(o2) != 1) {
+                return 1;
+            }
+            return o1.compareTo(o2);
+        }
     }
 }
